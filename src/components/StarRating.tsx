83b55ff3c9c8834d5c32970eb72bac40ec831a7d
Zhenyapas <78728092+Zhenyapas@users.noreply.github.com>
2022-11-28 21:10:16 +0200

    Implemented simple rating for Card component

--- src/components/StarRating.tsx
+++ src/components/StarRating.tsx
@@ -3,17 +3,29 @@ import {ReactComponent as Rate} from '../img/rate.svg';
 
 const StarRating = () => {
 
-    return (
 
-     <div className='flex lg:self-center lg:ml-8  lg:mb-6 xl:ml-28'>
-        <Rate className='w-[10px] lg:w-[19px] ml-[1px] fill-[#384564]/80  lg:fill-[#38415D]' />
-        <Rate className='w-[10px] lg:w-[19px] fill-[#384564]/80 lg:fill-[#38415D]' />
-        <Rate className='w-[10px] lg:w-[19px] fill-[#384564]/80 lg:fill-[#38415D]'  />
-        <Rate className='w-[10px] lg:w-[19px] fill-[#384564]/80 lg:fill-[#38415D]' />
-        <Rate className='w-[10px] lg:w-[19px]  ml-[1px] fill-[#384564]/80 opacity-70  lg:fill-[#38415D]' />
-     </div>
 
+    
+    const raiting = Math.floor(Math.random() * 6);
+    let arr = []
+
+   for(let i=0;i<raiting;i++){
+    arr.push(<Rate className='w-[10px] lg:w-[19px] ml-[1px] fill-[#384564]/80  lg:fill-[#38415D]'></Rate>)
+   };
+
+   const rest = 5 - raiting;
+
+   for(let i=0;i<rest;i++){
+    arr.push(<Rate className='w-[10px] lg:w-[19px]  ml-[1px] fill-[#384564]/80 opacity-70  lg:fill-[#38415D]' />)
+   };
+
+
+     return (
+        <div className='flex lg:self-center lg:ml-8  lg:mb-6 xl:ml-28'>
+            {arr}
+        </div>
     )
+
 }
 
 export default StarRating;
